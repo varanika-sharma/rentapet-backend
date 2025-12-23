7ecdcc6d36051bdb2267cfbbe9bd1d3993c9bda1
Varanika Sharma <varanikasharma1@gmail.com>
2025-12-23 08:18:38 -0500

    Initial Commit

---
+++ Rent a Pet/Rent_a_PetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rent_a_PetApp.swift
+//  Rent a Pet
+//
+//  Created by Loranika Sharma on 12/23/25.
+//
+
+import SwiftUI
+
+@main
+struct Rent_a_PetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
